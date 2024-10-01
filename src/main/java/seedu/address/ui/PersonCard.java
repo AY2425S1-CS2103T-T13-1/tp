bc80ccbc7e77178353c240b20f77ca67905f28b4
aldentantan <aldentanfk@gmail.com>
2024-10-01 14:13:37 +0800

    Update checkstyle

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -16,11 +16,6 @@ public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
 
-    @FXML
-    private Label remark;
-
-
-
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
@@ -28,8 +23,6 @@ public class PersonCard extends UiPart<Region> {
      *
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
-
-    public final Person person;
 
     @FXML
     private HBox cardPane;
@@ -44,7 +37,11 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label remark;
+    @FXML
     private FlowPane tags;
+
+    public final Person person;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
